ac65724549acf11291b12cdc7c8b8fb03014da34
Kenny <kenny.thorson@gmail.com>
2026-03-22 18:17:32 -0500

    icons now show up

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@300;400;700;800&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet" />
+      </head>
       <body>
         <div className="scanline" />
         <Header />
